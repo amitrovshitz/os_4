15d01866a059487ddb018dbb46a205cdba2340b6
amitrovshitz <amitrovshitz@gmail.com>
2023-05-23 00:16:30 +0300

    change the st_reactor

--- st_reactor.c
+++ st_reactor.c
@@ -8,89 +8,70 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void *reactorRun(void *this) {
+void *function_to_run(void *this) {
 	if (this == NULL)
 	{
 		return NULL;
 	}
 
 	t_reactor_ptr reactor = (t_reactor_ptr)this;
-
+    if(reactor == NULL)
+    {
+        return NULL;
+    }
 	while (reactor->run)
 	{
-		size_t size = 0, i = 0;
+		size_t size = 0;
 		node_reactor_ptr temp = reactor->head;
-
-		while (temp != NULL)
+		while(temp != NULL)
 		{
 			size++;
 			temp = temp->next;
 		}
-
-		temp = reactor->head;
-
 		reactor->fds = (pollfd_t_ptr)calloc(size, sizeof(pollfd_t));
-
 		if (reactor->fds == NULL)
 		{
 			return NULL;
 		}
-
-		while (temp != NULL)
+        temp = reactor->head;
+        size_t i = 0;
+		while(temp != NULL)
 		{
-			(*(reactor->fds + i)).fd = temp->fd;
-			(*(reactor->fds + i)).events = POLLIN;
-
+			reactor->fds[i].fd = temp->fd;
+			reactor->fds[i].events = POLLIN;
 			temp = temp->next;
 			i++;
 		}
-
 		int ret = poll(reactor->fds, i, -1);
-
-		if (ret < 0)
+		if (ret <= 0)
 		{
 			free(reactor->fds);
 			reactor->fds = NULL;
 			return NULL;
 		}
-
-		else if (ret == 0)
-		{
-			free(reactor->fds);
-			reactor->fds = NULL;
-			continue;
-		}
-
 		for (i = 0; i < size; ++i)
 		{
-			if ((*(reactor->fds + i)).revents & POLLIN)
+			if ((reactor->fds[i]).revents & POLLIN)
 			{
 				node_reactor_ptr temp = reactor->head;
-
 				for (unsigned int j = 0; j < i; ++j)
 					temp = temp->next;
-
 				void *handler_ret = temp->hdlr.handler((*(reactor->fds + i)).fd, reactor);
 
 				if (handler_ret == NULL && (*(reactor->fds + i)).fd != reactor->head->fd)
 				{
 					node_reactor_ptr curr_node = reactor->head;
 					node_reactor_ptr prev_node = NULL;
-
 					while (curr_node != NULL && curr_node->fd != (*(reactor->fds + i)).fd)
 					{
 						prev_node = curr_node;
 						curr_node = curr_node->next;
 					}
-
 					prev_node->next = curr_node->next;
-
 					free(curr_node);
 				}
-
 				continue;
 			}
-
 			else if (((*(reactor->fds + i)).revents & POLLHUP || (*(reactor->fds + i)).revents & POLLNVAL || (*(reactor->fds + i)).revents & POLLERR) && (*(reactor->fds + i)).fd != reactor->head->fd)
 			{
 				node_reactor_ptr curr_node = reactor->head;
@@ -101,18 +82,13 @@ void *reactorRun(void *this) {
 					prev_node = curr_node;
 					curr_node = curr_node->next;
 				}
-
 				prev_node->next = curr_node->next;
-
 				free(curr_node);
 			}
 		}
-
 		free(reactor->fds);
 		reactor->fds = NULL;
 	}
-
-
 	return reactor;
 }
 
@@ -151,9 +127,9 @@ void startReactor(void *this) {
 
 	reactor->run = true;
 
-	int ret_val = pthread_create(&reactor->thread, NULL, reactorRun, this);
+	int return_value = pthread_create(&reactor->thread, NULL, function_to_run, this);
 
-	if (ret_val != 0)
+	if (return_value != 0)
 	{
 		reactor->run = false;
 		reactor->thread = 0;
@@ -167,45 +143,29 @@ void stopReactor(void *this) {
 	{
 		return;
 	}
-
 	t_reactor_ptr reactor = (t_reactor_ptr)this;
-	void *ret = NULL;
-
+	void *thread_return = NULL;
 	if (!reactor->run)
 	{
 		return;
 	}
-
-
 	reactor->run = false;
-
-	int ret_val = pthread_cancel(reactor->thread);
-
-	if (ret_val != 0)
+	int return_value = pthread_cancel(reactor->thread);
+	if (return_value != 0)
 	{
 		return;
 	}
-
-	ret_val = pthread_join(reactor->thread, &ret);
-
-	if (ret_val != 0)
+	return_value = pthread_join(reactor->thread, &thread_return);
+	if (return_value != 0 || thread_return == NULL)
 	{
 		return;
-	}
-
-	if (ret == NULL)
-	{
-		return;
-	}
-
+    }
 	if (reactor->fds != NULL)
 	{
 		free(reactor->fds);
 		reactor->fds = NULL;
 	}
-	
 	reactor->thread = 0;
-
 }
 
 void addFd(void *this, int fd, handler_t handler) {
@@ -213,33 +173,24 @@ void addFd(void *this, int fd, handler_t handler) {
 	{
 		return;
 	}
-
-
 	t_reactor_ptr reactor = (t_reactor_ptr)this;
 	node_reactor_ptr node = (node_reactor_ptr)malloc(sizeof(node_reactor));
-
-	if (node == NULL)
+    if (node == NULL)
 	{
 		return;
 	}
-
-	node->fd = fd;
 	node->hdlr.handler = handler;
 	node->next = NULL;
-
-	if (reactor->head == NULL)
+    node->fd = fd;
+    if (reactor->head == NULL)
 		reactor->head = node;
-
-	else
+    else
 	{
-		node_reactor_ptr curr = reactor->head;
-
-		while (curr->next != NULL)
-			curr = curr->next;
-
-		curr->next = node;
+		node_reactor_ptr temp = reactor->head;
+	    while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = node;
 	}
-
 }
 
 void WaitFor(void *this) {
@@ -249,15 +200,14 @@ void WaitFor(void *this) {
 	}
 
 	t_reactor_ptr reactor = (t_reactor_ptr)this;
-	void *ret = NULL;
+	void *thread_return = NULL;
 
 	if (!reactor->run)
 		return;
 
-
-	int ret_val = pthread_join(reactor->thread, &ret);
+    int return_value = pthread_join(reactor->thread, &thread_return);
 	
-	if (ret_val != 0)
+	if (return_value != 0)
 	{
 		return;
 	}
